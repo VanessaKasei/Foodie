15de4675eb8d65e53f60ba33a000e067574de0fa
VanessaKasei <kaseivanessa@gmail.com>
2023-10-27 22:28:18 +0300

    aded color change bootm nav items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/pages/favorites_page.dart';
+import 'package:my_flutter_app/pages/profile_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,22 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void signUserOut() {
-    FirebaseAuth.instance.signOut();
-  }
+  int _selectedIndex = 0; // Track the selected item
 
-  @override
+  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         elevation: 0,
         backgroundColor: Color.fromARGB(255, 198, 219, 2),
-        title: Center(
-          child: Text("Discover. Savor. Repeat"),
-        ),
+        title: Text("Discover. Savor. Repeat"),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Color.fromARGB(255, 198, 219, 2),
+        bottomNavigationBar: BottomNavigationBar(
+          
+          elevation: 0,
+        /*selectedItemColor: Colors.cyan,
+        unselectedItemColor: Color.fromARGB(255, 198, 219, 2),*/
         items: [
           //home
           BottomNavigationBarItem(
@@ -44,15 +45,78 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.person),
             label: "Profile",
           ),
+        ],
+        onTap: (int index) {
+          if (index == 2) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => ProfilePage()),
+            );
+          } else if (index == 0) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          } else {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => FavoritesPage()),
+            );
+          }
+        },
+      ),
+    );
+  }
+}*/
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 198, 219, 2),
+        title: Text("Discover. Savor. Repeat"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        selectedItemColor: Colors.cyan, // Color for the selected item
+        unselectedItemColor: Color.fromARGB(255, 198, 219, 2), // Color for unselected items
+        currentIndex: _selectedIndex, // Set the selected index
+        items: [
+          // Home
           BottomNavigationBarItem(
-            //onTap: signUserOut,
-            icon: Icon(Icons.logout),
-            label: "Sign Out",
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+
+          // Favorites
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorites",
+          ),
+
+          // Profile
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
           ),
         ],
         onTap: (int index) {
-          if (index == 3) {
-            signUserOut();
+          // Handle item selection here
+          setState(() {
+            _selectedIndex = index; // Update the selected index
+          });
+
+          if (index == 2) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => ProfilePage()),
+            );
+          } /*else if (index == 0) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          */ else {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => FavoritesPage()),
+            );
           }
         },
       ),
